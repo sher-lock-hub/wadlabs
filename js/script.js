dce60c0d1e640d5906ef54378b2632c2f9a2afb7
Shalyn Njeri <nduta.shalyn@strathmore.edu>
2026-06-04 12:01:47 +0300

    JS 101

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let age=20;
+var Course="BBIT";
+const university="Strathmore";
+
+/*Redeclare */
+/*let age=20;*/
+var Course="BBIT";
+const university="Strathmore";
+
+/*Reassign */
+let age=19;
+var Course="BCOM";
+const university="Daystar";
+
+if(true){
+let time="11:30";
+var day="4th";
+const year=2026;
+}
+console.log(time);
